c3bc3880458cbcbb1364dd230ff078ce9de02e94
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-16 22:04:07 +0530

    initial commit of both files

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndStruct
+{
+    public class Person
+    {
+        //classes are reference types and are stored on Heap
+        //while structs are value types and are stored on Stack
+        //when we pass a value type, we are passing a copy of the object or value itself
+        //so changing one does not affect the other
+        //when we pass reference type, we are passing a reference to that object
+
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+
+        public struct PersonStruct
+        {
+            public string Name { get; set; }
+
+            public PersonStruct(string name)
+            {
+                Name = name;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ClassAndStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //creating intance for person class
+            Person person = new Person("Ram");
+
+            //creating instance for struct
+            Person personStruct = new Person("Shyam");
+
+
+            //assigning the person object to newPerson
+            var newPerson = person;
+
+            //modifying the name of newPerson
+            newPerson.Name = "Name Changed";
+
+
+            //Because "person" & "newPerson" are references to the same object
+            Console.WriteLine(person.Name); // Name Changed
+
+            var newPersonStruct = personStruct;
+            newPerson.Name = "Name Changed";
+
+            //Because "personStruct" and "newPersonStruct" are value types
+            Console.WriteLine(personStruct.Name);
+        }
+    }
+}
